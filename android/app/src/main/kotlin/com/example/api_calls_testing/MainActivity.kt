3ba8380635cd53b1e0533076f66406bbad9c57bb
SHIHAB69 <sihab2305341203@diu.edu.bd>
2025-03-01 16:05:42 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/api_calls_testing/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.api_calls_testing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AI Chat App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.grey[100],
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
